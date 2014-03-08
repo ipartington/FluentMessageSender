a5f3aed8ff9a82aecb75b326024d6fdf1060eff4
Iain Partington <ipartington@gmail.com>
2014-03-08 13:00:15 +0000

    Rename project

---
+++ FluentMessageSender/FluentMessager.vb
@@ -0,0 +1,10 @@
+﻿Imports FluentMessageSender.Fluent
+Public Class FluentMessager
+    Public Sub SendMessage(ByVal sender As MessageSender)
+
+        sender.SendText("Hi").From("Iain").To("Me")
+        sender.SendText("").From("").To("")
+        sender.SendText("").From(FromAddress.Iain).To("iain")
+
+    End Sub
+End Class
